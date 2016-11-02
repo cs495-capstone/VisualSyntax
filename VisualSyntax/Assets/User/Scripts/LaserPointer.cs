1d6086581612d0a0d14dea77d5eb00b21b930fe8
dkpowell1 <dkpowell1@catamount.wcu.edu>
2016-11-02 18:28:43 -0400

    added terrain and objects

---
+++ VisualSyntax/Assets/User/Scripts/LaserPointer.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// This script shows the user where they are looking using a game object. It kind
+/// of acts like a laser pointer because the object is placed directly where the 
+/// camera that this script is attached to is looking.
+/// </summary>
+public class LaserPointer : MonoBehaviour {
+
+	/// <summary>
+	/// This is the game object used to show the user where they are looking/where
+	/// they will teleport.
+	/// </summary>
+	public GameObject indicator;
+
+	/// <summary>
+	/// This is where rays casted from the camera's position intersect with the world.
+	/// This is the vector we set to be the indicator's position.
+	/// </summary>
+	private Vector3 hitPos;
+
+	/// <summary>
+	/// This method is called when the game is initially run. If the indicator
+	/// is not set from the Unity panel, then it will automatically create a 
+	/// cube to use as the indicator.
+	/// </summary>
+	void Start () {
+		if (indicator == null) {
+			indicator = GameObject.CreatePrimitive (PrimitiveType.Cube);
+			indicator.name = "indicator";
+		}
+	}
+
+	/// <summary>
+	/// This method gets called once per frame and just updates the location of the indicator
+	/// object so that the user can see where they are looking/where they will be able to 
+	/// teleport to.
+	/// </summary>
+	void Update () {
+		RaycastHit hit;
+
+		// Shoot a ray out from the camera using its angle and position
+		Ray ray = new Ray (transform.position, transform.forward);
+
+		// See if the ray intersects with the world
+		if (Physics.Raycast (ray, out hit)) {
+			// Only set the indicator if the ray hits terrain. This prevents teleporting into the sky
+			if (hit.collider.name == "Terrain" && hitPos != hit.point) {
+				hitPos = hit.point;
+				indicator.transform.position = hitPos;
+			}
+		}
+	}
+}
---
+++ VisualSyntax/Assets/User/Scripts/PlayerScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+public class PlayerScript : MonoBehaviour
+{
+	public GameObject prefab;
+
+    private SteamVR_TrackedObject trackedObj;
+    private FixedJoint joint;
+	private bool connected;
+	private GameObject go;
+	private Vector3 oldPos;
+
+
+    void Start()
+    {
+		connected = false;
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+		oldPos = transform.position;
+    }
+
+    void Update()
+    {
+        var device = SteamVR_Controller.Input((int)trackedObj.index);
+
+		SteamVR_TrackedController controller = GetComponent<SteamVR_TrackedController>();
+
+		if (!connected && device.GetTouchDown (SteamVR_Controller.ButtonMask.Trigger)) {
+			go = GameObject.Instantiate (prefab);
+			go.transform.position = trackedObj.transform.position;
+
+			var vel = transform.position - oldPos;
+			go.GetComponent<Rigidbody> ().velocity = (vel.magnitude + 200) * transform.forward;
+			connected = true;
+		} else if (connected) {
+			go.transform.position = trackedObj.transform.position;
+		}
+
+		if (!device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger)) {
+			connected = false;
+		}
+    }
+}
