4023ec35ac6028d25ca36d237a7fa3d2832bd34e
Anthony Benavente <anthony.m.benavente@gmail.com>
2016-11-06 21:57:00 -0500

    Added visualization of objects and links to other objects

---
+++ VisualSyntax/Assets/DrawLine.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrawLine : MonoBehaviour {
+
+	public Transform origin;
+	public Transform destination;
+
+	private LineRenderer lineRenderer;
+
+	// Use this for initialization
+	void Start () {
+		lineRenderer = GetComponent<LineRenderer> ();
+		lineRenderer.SetPosition (0, origin.position);
+		lineRenderer.SetWidth (0.01f, 0.01f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (origin != null && destination != null) {
+			lineRenderer.SetPosition (1, destination.position);
+		}
+	}
+}
