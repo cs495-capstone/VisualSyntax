209d3716d533e5dfe2e9f1227758fad6c8ab8358
Anthony Benavente <anthony.m.benavente@gmail.com>
2016-11-06 22:09:49 -0500

    Added documentation

--- VisualSyntax/Assets/DrawLine.cs
+++ VisualSyntax/Assets/DrawLine.cs
@@ -3,19 +3,33 @@ using System.Collections;
 
 public class DrawLine : MonoBehaviour {
 
+	/// <summary>
+	/// This is the origin of the line.
+	/// </summary>
 	public Transform origin;
+
+	/// <summary>
+	/// This is the end-point of the line
+	/// </summary>
 	public Transform destination;
 
 	private LineRenderer lineRenderer;
 
-	// Use this for initialization
+	/// <summary>
+	/// This method initializes the script. We set the line renderer's origin position
+	/// to the specified object and then shrink the size of the line to be manageable in
+	/// the world.
+	/// </summary>
 	void Start () {
 		lineRenderer = GetComponent<LineRenderer> ();
 		lineRenderer.SetPosition (0, origin.position);
 		lineRenderer.SetWidth (0.01f, 0.01f);
 	}
-	
-	// Update is called once per frame
+
+	/// <summary>
+	/// This update method sets the line render's end-point to the destination transform
+	/// specified in the field. This gets updated once per frame that the game is running.
+	/// </summary>
 	void Update () {
 		if (origin != null && destination != null) {
 			lineRenderer.SetPosition (1, destination.position);
